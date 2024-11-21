395fcbbbf9e67b7feda5c4107634af15b3f31888
Lucas Hen <oisouhen@gmail.com>
2024-11-21 13:48:50 -0300

    Update garages.lua

--- Server/resources/[scripts]/garages/server/garages.lua
+++ Server/resources/[scripts]/garages/server/garages.lua
@@ -33,44 +33,13 @@ Garages = {
 	["94"] = { name = "Aztecas", payment = false, perm = "Aztecas" },
 	["95"] = { name = "Bloods", payment = false, perm = "Bloods" },
 
-	-- Bikes
-	["101"] = { name = "Bikes", payment = false, license = false },
-	["102"] = { name = "Bikes", payment = false, license = false },
-	["103"] = { name = "Bikes", payment = false, license = false },
-	["104"] = { name = "Bikes", payment = false, license = false },
-	["105"] = { name = "Bikes", payment = false, license = false },
-	["106"] = { name = "Bikes", payment = false, license = false },
-	["107"] = { name = "Bikes", payment = false, license = false },
-	["108"] = { name = "Bikes", payment = false, license = false },
-	["109"] = { name = "Bikes", payment = false, license = false },
-	["110"] = { name = "Bikes", payment = false, license = false },
-	["111"] = { name = "Bikes", payment = false, license = false },
-	["112"] = { name = "Bikes", payment = false, license = false },
-	["113"] = { name = "Bikes", payment = false, license = false },
-	["114"] = { name = "Bikes", payment = false, license = false },
-	["115"] = { name = "Bikes", payment = false, license = false },
-	["116"] = { name = "Bikes", payment = false, license = false },
-	["117"] = { name = "Bikes", payment = false, license = false },
-	["118"] = { name = "Bikes", payment = false, license = false },
-
-	-- Boats
-	["121"] = { name = "Boats", payment = true, license = false },
-	["123"] = { name = "Boats", payment = true, license = false },
-	["124"] = { name = "Boats", payment = true, license = false },
-	["125"] = { name = "Boats", payment = true, license = false },
-	["126"] = { name = "Boats", payment = true, license = false },
-
 	-- Works
 	["141"] = { name = "Lumberman", payment = false, license = false },
 	["142"] = { name = "Driver", payment = false, license = false },
 	["143"] = { name = "Garbageman", payment = false, license = false },
 	["144"] = { name = "Transporter", payment = false, license = false },
 	["146"] = { name = "TowDriver", payment = false, license = false },
-	["148"] = { name = "Garbageman", payment = false, license = false },
-	["149"] = { name = "Garbageman", payment = false, license = false },
 	["150"] = { name = "Taxi", payment = false, license = false },
 	["151"] = { name = "Trucker", payment = false, license = false },
-	["152"] = { name = "Corrections", payment = false, license = false, perm = "Policia" },
-	["153"] = { name = "PostOp", payment = false, license = false },
-	["154"] = { name = "Farmer", payment = false, license = false }
+	["153"] = { name = "GoPostal", payment = false, license = false }
 }

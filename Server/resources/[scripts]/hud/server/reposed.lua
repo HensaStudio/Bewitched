2e0e8d50f3f7451368c38f049810f27426183dd8
Lucas Hen <oisouhen@gmail.com>
2025-02-04 13:47:55 -0300

    Update reposed.lua

--- Server/resources/[scripts]/hud/server/reposed.lua
+++ Server/resources/[scripts]/hud/server/reposed.lua
@@ -1,6 +1,7 @@
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
+local Call = {}
 local Reposed = {}
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- REPOSED
@@ -21,11 +22,22 @@ end)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- REPOSED
 -----------------------------------------------------------------------------------------------------------------------------------------
-exports("Reposed", function(Passport)
+exports("Reposed", function(Passport, source)
+	local source = parseInt(source)
 	local Passport = parseInt(Passport)
 
 	if Reposed[Passport] then
 		if Reposed[Passport] > os.time() then
+			if not Call[Passport] then
+				Call[Passport] = os.time()
+			end
+
+			if Call[Passport] <= os.time() and source > 0 then
+				Call[Passport] = os.time() + 60
+
+				TriggerClientEvent("Notify", source, "Hospital", "Você está de repouso e não pode fazer isso agora.", "hospital", 10000)
+			end
+
 			return true
 		end
 	end

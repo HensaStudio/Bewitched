a6e26f82ff8fad84a28e1afb106a5f598df4baa5
Lucas Hen <oisouhen@gmail.com>
2025-03-27 12:31:20 -0300

    added Taximeter

---
+++ Server/resources/[scripts]/taximeter/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+ui_page "web/index.html"
+
+client_scripts {
+	"client/*"
+}
+
+files {
+	"web/*",
+	"web/**/*"
+}
+
+shared_scripts {
+	"shared/*"
+}

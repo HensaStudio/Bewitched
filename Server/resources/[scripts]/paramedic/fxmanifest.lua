fe0965dba2ce9ded0af1a7e23c29ea48e641eec4
Lucas Hen <oisouhen@gmail.com>
2025-03-28 10:59:09 -0300

    Update fxmanifest.lua

--- Server/resources/[scripts]/paramedic/fxmanifest.lua
+++ Server/resources/[scripts]/paramedic/fxmanifest.lua
@@ -2,8 +2,6 @@ fx_version "bodacious"
 game "gta5"
 lua54 "yes"
 
-ui_page "web/index.html"
-
 client_scripts {
 	"@vrp/config/Native.lua",
 	"@PolyZone/client.lua",
@@ -12,15 +10,8 @@ client_scripts {
 }
 
 server_scripts {
-	"@vrp/config/Global.lua",
-	"@vrp/config/Item.lua",
 	"@vrp/lib/Utils.lua",
 	"server/*"
-}
-
-files {
-	"web/*",
-	"web/**/*"
 }
 
 shared_scripts {

795f5b3273529cd02cc588a65f976fdaba99eea8
Lucas Hen <oisouhen@gmail.com>
2025-04-28 09:58:06 -0300

    Update fxmanifest.lua

--- Server/resources/[maps]/cfx-hensa/fxmanifest.lua
+++ Server/resources/[maps]/cfx-hensa/fxmanifest.lua
@@ -1,22 +1,22 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-this_is_a_map "yes"
+this_is_a_map 'yes'
 
 client_scripts {
-	"client/*"
+	'client/*'
 }
 
 files {
-	"stream/*",
-	"stream/**/*",
-	"stream/**/**/*",
-	"stream/**/**/**/*",
-	"interiorproxies.meta"
+	'stream/*',
+	'stream/**/*',
+	'stream/**/**/*',
+	'stream/**/**/**/*'
 }
 
-data_file "DLC_ITYP_REQUEST" "stream/*.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/**/*.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/**/**/*.ytyp"
-data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/**/**/*.ytyp'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'data/interiorproxies.meta'

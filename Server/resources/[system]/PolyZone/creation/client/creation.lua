8db48d2fcb75993ee41c86e3248bbba59bcf7fce
Lucas Hen <oisouhen@gmail.com>
2025-05-02 13:04:23 -0300

    Update creation.lua

--- Server/resources/[system]/PolyZone/creation/client/creation.lua
+++ Server/resources/[system]/PolyZone/creation/client/creation.lua
@@ -5,8 +5,13 @@ createdZone = nil
 drawZone = false
 
 RegisterNetEvent("polyzone:pzcreate")
-AddEventHandler("polyzone:pzcreate", function(zoneType, name, Message)
-  if createdZone then
+AddEventHandler("polyzone:pzcreate", function(zoneType, name, args)
+  if createdZone ~= nil then
+    TriggerEvent('chat:addMessage', {
+      color = { 255, 0, 0},
+      multiline = true,
+      args = {"Me", "A shape is already being created!"}
+    })
     return
   end
 
@@ -14,23 +19,38 @@ AddEventHandler("polyzone:pzcreate", function(zoneType, name, Message)
     polyStart(name)
   elseif zoneType == "circle" then
     local radius = nil
-    if #Message >= 3 then radius = tonumber(Message[3])
+    if #args >= 3 then radius = tonumber(args[3])
     else radius = tonumber(GetUserInput("Enter radius:")) end
-    if not radius then
+    if radius == nil then
+      TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = true,
+        args = {"Me", "CircleZone requires a radius (must be a number)!"}
+      })
       return
     end
     circleStart(name, radius)
   elseif zoneType == "box" then
     local length = nil
-    if #Message >= 3 then length = tonumber(Message[3])
+    if #args >= 3 then length = tonumber(args[3])
     else length = tonumber(GetUserInput("Enter length:")) end
-    if not length or length < 0.0 then
+    if length == nil or length < 0.0 then
+      TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = true,
+        args = {"Me", "BoxZone requires a length (must be a positive number)!"}
+      })
       return
     end
     local width = nil
-    if #Message >= 4 then width = tonumber(Message[4])
+    if #args >= 4 then width = tonumber(args[4])
     else width = tonumber(GetUserInput("Enter width:")) end
-    if not width or width < 0.0 then
+    if width == nil or width < 0.0 then
+      TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0},
+        multiline = true,
+        args = {"Me", "BoxZone requires a width (must be a positive number)!"}
+      })
       return
     end
     boxStart(name, 0, length, width)
@@ -45,7 +65,7 @@ end)
 
 RegisterNetEvent("polyzone:pzfinish")
 AddEventHandler("polyzone:pzfinish", function()
-  if not createdZone then
+  if createdZone == nil then
     return
   end
 
@@ -57,6 +77,12 @@ AddEventHandler("polyzone:pzfinish", function()
     boxFinish()
   end
 
+  TriggerEvent('chat:addMessage', {
+    color = { 0, 255, 0},
+    multiline = true,
+    args = {"Me", "Check PolyZone's root folder for polyzone_created_zones.txt to get the zone!"}
+  })
+
   lastCreatedZoneType = createdZoneType
   lastCreatedZone = createdZone
 
@@ -67,12 +93,19 @@ end)
 
 RegisterNetEvent("polyzone:pzlast")
 AddEventHandler("polyzone:pzlast", function()
-  if createdZone or not lastCreatedZone then
+  if createdZone ~= nil or lastCreatedZone == nil then
     return
+  end
+  if lastCreatedZoneType == 'poly' then
+    TriggerEvent('chat:addMessage', {
+      color = { 0, 255, 0},
+      multiline = true,
+      args = {"Me", "The command pzlast only supports BoxZone and CircleZone for now"}
+    })
   end
 
   local name = GetUserInput("Enter name (or leave empty to reuse last zone's name):")
-  if not name then
+  if name == nil then
     return
   elseif name == "" then
     name = lastCreatedZone.name
@@ -97,9 +130,15 @@ end)
 
 RegisterNetEvent("polyzone:pzcancel")
 AddEventHandler("polyzone:pzcancel", function()
-  if not createdZone then
+  if createdZone == nil then
     return
   end
+
+  TriggerEvent('chat:addMessage', {
+    color = {255, 0, 0},
+    multiline = true,
+    args = {"Me", "Zone creation canceled!"}
+  })
 
   drawZone = false
   createdZone = nil
@@ -108,10 +147,10 @@ end)
 
 -- Drawing
 function drawThread()
-  CreateThread(function()
+  Citizen.CreateThread(function()
     while drawZone do
       if createdZone then
-        createdZone:draw()
+        createdZone:draw(true)
       end
       Wait(0)
     end

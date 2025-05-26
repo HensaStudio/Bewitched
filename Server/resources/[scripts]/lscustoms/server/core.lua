78e9f61c25454a4622764c209feb134c625e2d92
Lucas Hen <oisouhen@gmail.com>
2025-05-26 09:24:27 -0300

    Update core.lua

--- Server/resources/[scripts]/lscustoms/server/core.lua
+++ Server/resources/[scripts]/lscustoms/server/core.lua
@@ -3,7 +3,6 @@
 -----------------------------------------------------------------------------------------------------------------------------------------
 local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
-vRPC = Tunnel.getInterface("vRP")
 vRP = Proxy.getInterface("vRP")
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CONNECTION
@@ -13,57 +12,96 @@ Tunnel.bindInterface("lscustoms",Hensa)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
-local InVehicle = {}
+local Networked = {}
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- PERMISSION
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Hensa.Permission(Index)
 	local source = source
 	local Passport = vRP.Passport(source)
-	local Permission = Locations[Index]["Permission"]
-	if Passport then
-		if not Permission then
-			return true
-		else
-			if vRP.HasService(Passport,Permission) then
-				return true
-			else
-				TriggerClientEvent("Notify", source, "Atenção", "Você não tem permissões.", "amarelo", 5000)
-			end
-		end
-	end
 
-	return false
+	return Passport and Locations[Index] and (not Locations[Index]["Permission"] or (Locations[Index]["Permission"] and vRP.HasService(Passport,Locations[Index]["Permission"]))) and true or false
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- SAVE
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.Save(Model,Plate,Mods,Price)
+function Hensa.Save(Model,Plate,Initial)
+	local Return = false
 	local source = source
 	local Passport = vRP.Passport(source)
-	if Passport then
-		if vRP.PaymentFull(Passport,Price) then
-			local TheVehicle = vRP.Query("vehicles/plateVehicles", { Plate = Plate })
-			if TheVehicle[1] then
-				vRP.Query("entitydata/SetData",{ Name = "LsCustoms:"..TheVehicle[1]["Passport"]..":"..Model..":"..TheVehicle[1]["id"], Information = json.encode(Mods) })
-				vRP.UpgradeStress(Passport, math.random(5))
+	local Price = Calculate(Initial,Model)
+	if Passport and (Price <= 0 or vRP.PaymentFull(Passport,Price,true)) then
+		local OtherPassport = vRP.PassportPlate(Plate)
+		if OtherPassport then
+			local Consult = vRP.GetServerData("LsCustoms:"..OtherPassport..":"..Model)
+			for Index,v in pairs(Initial) do
+				if Index == "VehicleExtras" then
+					for Type,Results in pairs(Initial[Index]) do
+						if Results["Installed"] ~= Results["Selected"] then
+							if not Consult[Index] then
+								Consult[Index] = {}
+							end
 
-				TriggerClientEvent("Notify", source, "Sucesso", "Você modificou um veículo registrado.", "verde", 5000)
-			else
-				if CallPolices then
-					TriggerClientEvent("Notify", source, "Atenção", "Você modificou um veículo sem registro e a polícia foi acionada.", "amarelo", 5000)
-
-					exports["vrp"]:CallPolice({ ["Source"] = source, ["Passport"] = Passport, ["Permission"] = "Policia", ["Name"] = "Possível veículo roubado", ["Code"] = 20, ["Color"] = 16 })
-				else
-					TriggerClientEvent("Notify", source, "Atenção", "Você modificou um veículo sem registro.", "amarelo", 5000)
+							Consult[Index][Type] = Results["Selected"]
+						end
+					end
+				elseif Index == "Respray" then
+					Consult[Index] = {
+						["PrimaryColour"] = {
+							["Type"] = Initial[Index]["PrimaryColour"]["Selected"]["Type"],
+							["Color"] = Initial[Index]["PrimaryColour"]["Selected"]["Color"]
+						},
+						["SecondaryColour"] = {
+							["Type"] = Initial[Index]["SecondaryColour"]["Selected"]["Type"],
+							["Color"] = Initial[Index]["SecondaryColour"]["Selected"]["Color"]
+						},
+						["PearlescentColour"] = Initial[Index]["PearlescentColour"]["Selected"],
+						["WheelColour"] = Initial[Index]["WheelColour"]["Selected"],
+						["DashboardColour"] = Initial[Index]["DashboardColour"]["Selected"],
+						["InteriorColour"] = Initial[Index]["InteriorColour"]["Selected"]
+					}
+				elseif Index == "Wheels" then
+					for Type,Results in pairs(Initial[Index]) do
+						if Results["Installed"] ~= Results["Selected"] then
+							if Consult[Index] then
+								if Type == "TyreSmoke" then
+									Consult[Index]["TyreSmoke"] = Initial[Index]["TyreSmoke"]["Selected"]
+								elseif Type == "CustomTyres" then
+									Consult[Index]["CustomTyres"] = Initial[Index]["CustomTyres"]["Selected"]
+								else
+									Consult[Index]["Category"] = Type
+									Consult[Index]["Value"] = Results["Selected"]
+								end
+							else
+								if Type == "TyreSmoke" then
+									Consult[Index] = {
+										["TyreSmoke"] = Initial[Index]["TyreSmoke"]["Selected"]
+									}
+								elseif Type == "CustomTyres" then
+									Consult[Index] = {
+										["CustomTyres"] = Initial[Index]["CustomTyres"]["Selected"]
+									}
+								else
+									Consult[Index] = {
+										["Category"] = Type,
+										["Value"] = Results["Selected"]
+									}
+								end
+							end
+						end
+					end
+				elseif v["Installed"] ~= v["Selected"] then
+					Consult[Index] = v["Selected"]
 				end
 			end
 
-			return true
+			vRP.SetServerData("LsCustoms:"..OtherPassport..":"..Model,Consult)
 		end
+
+		Return = true
 	end
 
-	return false
+	return Return
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- LSCUSTOMS:NETWORK
@@ -74,22 +112,22 @@ AddEventHandler("lscustoms:Network",function(Network,Plate)
 	local Passport = vRP.Passport(source)
 	if Passport then
 		if not Network then
-			if InVehicle[Passport] then
-				InVehicle[Passport] = nil
+			if Networked[Passport] then
+				Networked[Passport] = nil
 			end
 		else
-			InVehicle[Passport] = { Network,Plate }
+			Networked[Passport] = { Network,Plate }
 		end
 	end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- DISCONNECT
 -----------------------------------------------------------------------------------------------------------------------------------------
-AddEventHandler("Disconnect",function(Passport)
-	if InVehicle[Passport] then
-		SetTimeout(1000,function()
-			TriggerEvent("garages:DeleteVehicle",InVehicle[Passport][1],InVehicle[Passport][2])
-			InVehicle[Passport] = nil
+AddEventHandler("Disconnect",function(Passport,source)
+	if Networked[Passport] then
+		SetTimeout(2500,function()
+			TriggerEvent("garages:Deleted",Networked[Passport][1],Networked[Passport][2])
+			Networked[Passport] = nil
 		end)
 	end
 end)

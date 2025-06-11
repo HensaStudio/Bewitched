6e3364ce8d318398c4f8aeaa0c06af11b4a96ae4
Lucas Hen <oisouhen@gmail.com>
2025-06-11 12:13:07 -0300

    Create presets.lua

---
+++ Server/resources/[scripts]/player/server/presets.lua
@@ -0,0 +1,77 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- PRESETS
+-----------------------------------------------------------------------------------------------------------------------------------------
+Presets = {
+	["1"] = {
+        ["mp_m_freemode_01"] = {
+            ["hat"] = { item = -1, texture = 0 },
+            ["pants"] = { item = 195, texture = 0 },
+            ["vest"] = { item = 65, texture = 0 },
+            ["bracelet"] = { item = -1, texture = 0 },
+            ["backpack"] = { item = 0, texture = 0 },
+            ["decals"] = { item = 197, texture = 12 },
+            ["mask"] = { item = 0, texture = 0 },
+            ["shoes"] = { item = 25, texture = 0 },
+            ["tshirt"] = { item = 15, texture = 0 },
+            ["torso"] = { item = 548, texture = 0 },
+            ["accessory"] = { item = 183, texture = 0 },
+            ["watch"] = { item = -1, texture = 0 },
+            ["arms"] = { item = 22, texture = 0 },
+            ["glass"] = { item = 0, texture = 0 },
+            ["ear"] = { item = -1, texture = 0 }
+        },
+        ["mp_f_freemode_01"] = {
+            ["hat"] = { item = -1, texture = 0 },
+            ["pants"] = { item = 209, texture = 0 },
+            ["vest"] = { item = 63, texture = 0 },
+            ["bracelet"] = { item = -1, texture = 0 },
+            ["backpack"] = { item = 0, texture = 0 },
+            ["decals"] = { item = 209, texture = 12 },
+            ["mask"] = { item = 0, texture = 0 },
+            ["shoes"] = { item = 25, texture = 0 },
+            ["tshirt"] = { item = 254, texture = 0 },
+            ["torso"] = { item = 584, texture = 0 },
+            ["accessory"] = { item = 148, texture = 0 },
+            ["watch"] = { item = -1, texture = 0 },
+            ["arms"] = { item = 21, texture = 0 },
+            ["glass"] = { item = 0, texture = 0 },
+            ["ear"] = { item = -1, texture = 0 }
+        }
+	},
+	["2"] = {
+        ["mp_m_freemode_01"] = {
+            ["hat"] = { item = -1, texture = 0 },
+            ["pants"] = { item = 31, texture = 0 },
+            ["vest"] = { item = 0, texture = 0 },
+            ["bracelet"] = { item = -1, texture = 0 },
+            ["backpack"] = { item = 0, texture = 0 },
+            ["decals"] = { item = 58, texture = 1 },
+            ["mask"] = { item = 0, texture = 0 },
+            ["shoes"] = { item = 25, texture = 0 },
+            ["tshirt"] = { item = 154, texture = 0 },
+            ["torso"] = { item = 250, texture = 0 },
+            ["accessory"] = { item = 171, texture = 0 },
+            ["watch"] = { item = -1, texture = 0 },
+            ["arms"] = { item = 85, texture = 0 },
+            ["glass"] = { item = 0, texture = 0 },
+            ["ear"] = { item = -1, texture = 0 }
+        },
+        ["mp_f_freemode_01"] = {
+            ["hat"] = { item = -1, texture = 0 },
+            ["pants"] = { item = 30, texture = 0 },
+            ["vest"] = { item = 0, texture = 0 },
+            ["bracelet"] = { item = -1, texture = 0 },
+            ["backpack"] = { item = 0, texture = 0 },
+            ["decals"] = { item = 66, texture = 1 },
+            ["mask"] = { item = 0, texture = 0 },
+            ["shoes"] = { item = 25, texture = 0 },
+            ["tshirt"] = { item = 190, texture = 0 },
+            ["torso"] = { item = 258, texture = 0 },
+            ["accessory"] = { item = 96, texture = 0 },
+            ["watch"] = { item = -1, texture = 0 },
+            ["arms"] = { item = 109, texture = 0 },
+            ["glass"] = { item = 0, texture = 0 },
+            ["ear"] = { item = -1, texture = 0 }
+        }
+	}
+}

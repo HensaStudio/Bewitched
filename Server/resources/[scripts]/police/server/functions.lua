d48b2088e81eae97901b3fdaa883b416e11e867e
Lucas Hen <oisouhen@gmail.com>
2025-06-11 12:12:58 -0300

    Update functions.lua

--- Server/resources/[scripts]/police/server/functions.lua
+++ Server/resources/[scripts]/police/server/functions.lua
@@ -21,45 +21,6 @@ AddEventHandler("police:ArrestItens", function(Entity)
     end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
--- PRESET
------------------------------------------------------------------------------------------------------------------------------------------
-local Preset = {
-	["mp_m_freemode_01"] = {
-		["hat"] = { item = -1, texture = 0 },
-		["pants"] = { item = 59, texture = 1 },
-		["vest"] = { item = 0, texture = 0 },
-		["bracelet"] = { item = -1, texture = 0 },
-		["backpack"] = { item = 0, texture = 0 },
-		["decals"] = { item = 0, texture = 0 },
-		["mask"] = { item = 0, texture = 0 },
-		["shoes"] = { item = 25, texture = 0 },
-		["tshirt"] = { item = 15, texture = 0 },
-		["torso"] = { item = 5, texture = 0 },
-		["accessory"] = { item = 0, texture = 0 },
-		["watch"] = { item = -1, texture = 0 },
-		["arms"] = { item = 5, texture = 0 },
-		["glass"] = { item = 0, texture = 0 },
-		["ear"] = { item = -1, texture = 0 }
-	},
-	["mp_f_freemode_01"] = {
-		["hat"] = { item = -1, texture = 0 },
-		["pants"] = { item = 61, texture = 1 },
-		["vest"] = { item = 0, texture = 0 },
-		["bracelet"] = { item = -1, texture = 0 },
-		["backpack"] = { item = 0, texture = 0 },
-		["decals"] = { item = 0, texture = 0 },
-		["mask"] = { item = 0, texture = 0 },
-		["shoes"] = { item = 25, texture = 0 },
-		["tshirt"] = { item = 3, texture = 0 },
-		["torso"] = { item = 118, texture = 0 },
-		["accessory"] = { item = 0, texture = 0 },
-		["watch"] = { item = -1, texture = 0 },
-		["arms"] = { item = 11, texture = 0 },
-		["glass"] = { item = 0, texture = 0 },
-		["ear"] = { item = -1, texture = 0 }
-	}
-}
------------------------------------------------------------------------------------------------------------------------------------------
 -- POLICE:PRESET
 -----------------------------------------------------------------------------------------------------------------------------------------
 RegisterServerEvent("police:Preset")
@@ -69,7 +30,7 @@ AddEventHandler("police:Preset",function(OtherSource)
 	if Passport and vRP.GetHealth(source) > 100 and vRP.HasService(Passport,"Policia") then
 		local Hash = vRP.ModelPlayer(OtherSource)
 		if Hash == "mp_m_freemode_01" or Hash == "mp_f_freemode_01" then
-			TriggerClientEvent("skinshop:Apply",OtherSource,Preset[Hash])
+			TriggerClientEvent("skinshop:Apply",OtherSource,Presets[Hash])
 		end
 	end
 end)

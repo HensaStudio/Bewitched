8df5e8328b1a30bd44a66f6ef2ce80ad92a8866f
Lucas Hen <oisouhen@gmail.com>
2025-10-28 16:40:38 -0300

    Update core.lua

--- Server/resources/[maps]/cfx-gabz/client/core.lua
+++ Server/resources/[maps]/cfx-gabz/client/core.lua
@@ -1,63 +1,62 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- GABZINTERIORS
+-----------------------------------------------------------------------------------------------------------------------------------------
+local GabzInteriors = {
+	-- PILLBOX HOSPITAL
+	{
+		ipl = "gabz_pillbox_milo_",
+		coords = vec3(311.2546, -592.4204, 42.32737),
+		removeIpls = {
+			"rc12b_fixed",
+			"rc12b_destroyed",
+			"rc12b_default",
+			"rc12b_hospitalinterior_lod",
+			"rc12b_hospitalinterior"
+		}
+	},
+
+	-- IMPORT GARAGE
+	{
+		ipl = "gabz_imp_impexp_interior_placement_interior_1_impexp_intwaremed_milo_",
+		coords = vec3(941.0084, -972.6645, 39.14678),
+		props = { "branded_style_set", "car_floor_hatch" }
+	},
+
+	-- LSPD - MISSION ROW
+	{
+		ipl = "gabz_mrpd_milo_",
+		coords = vec3(451.0129, -993.3741, 29.1718),
+		props = (function()
+			local t = {}
+			for i = 1, 31 do t[#t+1] = "v_gabz_mrpd_rm"..i end
+			return t
+		end)()
+	}
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- GABZTHREAD
+-----------------------------------------------------------------------------------------------------------------------------------------
 CreateThread(function()
-	RequestIpl("gabz_pillbox_milo_")
+	for _, data in ipairs(GabzInteriors) do
+		if data.ipl then
+			RequestIpl(data.ipl)
+		end
 
-	local HospitalInterior = GetInteriorAtCoords(311.2546, -592.4204, 42.32737)
-	if IsValidInterior(HospitalInterior) then
-		RemoveIpl("rc12b_fixed")
-		RemoveIpl("rc12b_destroyed")
-		RemoveIpl("rc12b_default")
-		RemoveIpl("rc12b_hospitalinterior_lod")
-		RemoveIpl("rc12b_hospitalinterior")
+		local interior = GetInteriorAtCoords(data.coords.x, data.coords.y, data.coords.z)
+		if IsValidInterior(interior) then
+			if data.removeIpls then
+				for _, iplName in ipairs(data.removeIpls) do
+					RemoveIpl(iplName)
+				end
+			end
 
-		RefreshInterior(HospitalInterior)
-	end
+			if data.props then
+				for _, prop in ipairs(data.props) do
+					EnableInteriorProp(interior, prop)
+				end
+			end
 
-	RequestIpl("gabz_mrpd_milo_")
-
-	local PoliceInterior = GetInteriorAtCoords(451.0129, -993.3741, 29.1718)
-	if IsValidInterior(PoliceInterior) then      
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm1")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm2")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm3")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm4")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm5")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm6")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm7")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm8")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm9")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm10")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm11")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm12")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm13")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm14")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm15")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm16")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm17")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm18")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm19")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm20")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm21")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm22")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm23")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm24")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm25")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm26")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm27")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm28")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm29")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm30")
-		EnableInteriorProp(PoliceInterior, "v_gabz_mrpd_rm31")
-
-		RefreshInterior(PoliceInterior)
-	end
-
-	RequestIpl("gabz_imp_impexp_interior_placement_interior_1_impexp_intwaremed_milo_")
-
-	local ImportInterior = GetInteriorAtCoords(941.00840000, -972.66450000, 39.14678000)
-	if IsValidInterior(ImportInterior) then
-		EnableInteriorProp(ImportInterior, "branded_style_set")
-		EnableInteriorProp(ImportInterior, "car_floor_hatch")
-
-		RefreshInterior(ImportInterior)
+			RefreshInterior(interior)
+		end
 	end
 end)
